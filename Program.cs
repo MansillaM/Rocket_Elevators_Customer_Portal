5f10cfc4c860ad43dd0e31b270b10036ff79fe4a
Your Name <you@example.com>
2022-08-11 19:54:59 -0400

    working on code

---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Rocket_Elevators_Customer_Portal.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Rocket_Elevators_Customer_PortalIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'Rocket_Elevators_Customer_PortalIdentityDbContextConnection' not found.");
+
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+builder.Services.AddDbContext<Rocket_Elevators_Customer_PortalIdentityDbContext>(options =>
+    options.UseMySql(connectionString, serverVersion));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<Rocket_Elevators_Customer_PortalIdentityDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins(
+                                              "*"
+                                              )
+                                              .WithMethods("POST", "PUT", "DELETE", "GET")
+                                              .WithHeaders("*");
+                      });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
---
+++ wwwroot/js/site.js
@@ -0,0 +1,74 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function() {
+
+    hideAll();
+    onLoad();
+
+    function hideAll() {
+        // $("#intervention_building_id").hide();
+        $("#intervention_battery_id").hide();
+        $("#intervention_column_id").hide();
+        $("#intervention_elevator_id").hide();
+    }
+
+    function onLoad() {
+        let email2Id = $("#customer_email").val();
+        console.log(email2Id)
+        customerIdFromEmail(email2Id)
+    }
+    
+})
+
+function customerIdFromEmail(Email){
+    console.log(Email)
+    $.ajax({
+        url: "https://localhost:8888/api/customers/" + Email ,
+        header: {
+            "accept": "application/json",
+            "Access-Control-Allow-Origin" : "*"
+        },
+        type: "get",
+        crossDomain: true,
+        data: {
+            email: Email
+        },
+        dataType: "json",
+        success: function (customer) {
+            let customerEmail = $("#intervention_customer_id")
+            customerEmail.empty()
+            customerEmail.show()
+            $.each(customer, function() {
+                customerEmail.append("<option value=" + customer[0].id + ">" + customer[0].id + "</option>")
+            })
+            // buildingIdFromCustomer();
+            console.log('customer', customer)
+        },
+        error : function(jqXHR, textStatus, errorThrown){
+            console.log(textStatus)
+          }
+        
+    })
+}
+
+function buildingIdFromCustomer(customerId){
+    $.ajax({
+        url: "https://localhost:8888/api/buildings",
+        type: "get",
+        data: {
+            customer_id: customerId
+        },
+        dataType: "json",
+        success: function (building) {
+            let buildingCustomer = $("#intervention_building_id")
+            buildingCustomer.empty()
+            buildingCustomer.show()
+            $.each(building, function() {
+                buildingCustomer.append("<option value=" + building[0].id + ">" + building[0].id + "</option>")
+            })
+        }
+    })
+}
