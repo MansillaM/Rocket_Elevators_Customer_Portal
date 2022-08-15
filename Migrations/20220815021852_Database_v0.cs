6f7d5594e12cfd38bf6fff0a8661dd8ba8b13ab5
Your Name <you@example.com>
2022-08-14 22:21:56 -0400

    update db

---
+++ Migrations/20220815021852_Database_v0.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rocket_Elevators_Customer_Portal.Migrations
+{
+    public partial class Database_v0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
